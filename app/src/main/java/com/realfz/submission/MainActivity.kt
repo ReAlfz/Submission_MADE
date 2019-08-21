49d00db1172b25bf00949d83c36952b327b59a18
ReAlfz <alfianrudiyanto1@gmail.com>
2019-08-21 19:11:47 +0700

    Update MainActivity.kt

--- app/src/main/java/com/realfz/submission/MainActivity.kt
+++ app/src/main/java/com/realfz/submission/MainActivity.kt
@@ -5,17 +5,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onClick(p0: View?) {
+        val click = Intent(this, MainActivity::class.java)
+        startActivity(click)
+    }
 
     private lateinit var rvKS : RecyclerView
     private var list: ArrayList<Gem> = arrayListOf()
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val res = findViewById<TextView>(R.id.nama)
+        res.setOnClickListener(this)
 
         rvKS = findViewById(R.id.rv_AC)
         rvKS.setHasFixedSize(true)
